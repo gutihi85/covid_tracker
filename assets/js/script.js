6c1019fd4fd8cb4af14c9ac220b9c8c899ea03e5
Alejandra Gutierrez <gutihi85@gmail.com>
2021-04-22 22:00:28 -0500

    HTML + JS progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-
+var totalInfectedGlobal = document.getElementById("totalInfected-global")
+var newsHolder = document.getElementById("newsHolder")
 
 function getApi() {
 
-  var covidNewsUrl = `https://content.guardianapis.com/search?q=covid&api-key=962e64d3-a7ad-4bb9-8881-a000ce805d15`;
+  var covidNewsUrl = `https://content.guardianapis.com/search?q=covid&q=mexico&api-key=962e64d3-a7ad-4bb9-8881-a000ce805d15`;
 
  
   fetch(covidNewsUrl)
@@ -12,7 +13,27 @@ function getApi() {
     .then(function (newsData) {
       // sanity check
       console.log(newsData)
-      localStorage.setItem("newsData", JSON.stringify(newsData));
+
+    
+    var date = newsData.response.results[0].webPublicationDate;
+    var title = newsData.response.results[0].webTitle;
+    var newsUrl = newsData.response.results[0].webUrl;
+    console.log(date)
+    console.log(title)
+    console.log(newsUrl)
+    
+    // var newsElement = document.createElement("a");
+    // newsHolder.appendChild(newsElement);
+    // newsHolder.lastChild.textContent = title;
+    
+    for (var i = 0; i < newsData.response.results.length; i++){
+    var newsElement = document.createElement("a");
+    newsHolder.appendChild(newsElement);
+    newsHolder.lastChild.textContent = newsData.response.results[i].webTitle + newsData.response.results[i].webPublicationDate;
+    console.log(newsData.response.results[i].webPublicationDate);
+    console.log(newsData.response.results[i].webTitle);
+    console.log(newsData.response.results[i].webUrl);
+    }
     });
 
  var globalCasesUrl = `https://disease.sh/v3/covid-19/all?yesterday=false&twoDaysAgo=false&allowNull=true`;
@@ -24,10 +45,22 @@ function getApi() {
     .then(function (globalData) {
       // sanity check
       console.log(globalData)
-      localStorage.setItem("globalData", JSON.stringify(globalData));
+    var cases = globalData.cases;
+    totalInfectedGlobal.textContent = cases;
+    var deaths = globalData.deaths;
+    var recovered = globalData.recovered;
+    var todayCases = globalData.todayCases;
+    var todayDeaths = globalData.todayDeaths;
+    var todayRecovered = globalData.todayRecovered;
+    console.log(cases)
+    console.log(deaths)
+    console.log(recovered)
+    console.log(todayCases)
+    console.log(todayDeaths)
+    console.log(todayRecovered)
     });
 
- var countryCasesUrl = `https://disease.sh/v3/covid-19/countries?yesterday=true&twoDaysAgo=false&sort=cases&allowNull=true`;
+ var countryCasesUrl = `https://disease.sh/v3/covid-19/countries/mexico?yesterday=false&twoDaysAgo=false&strict=true&allowNull=true`;
 
    fetch(countryCasesUrl)
     .then(function (response3) {
@@ -36,9 +69,27 @@ function getApi() {
     .then(function (countryData) {
       // sanity check
       console.log(countryData)
-      localStorage.setItem("countryData", JSON.stringify(countryData));
-    });
 
+      var countryCases = countryData.cases;
+      var countryDeaths = countryData.deaths;
+      var countryRecovered = countryData.recovered;
+      var countryTodayCases = countryData.todayCases;
+      var countryTodayDeaths = countryData.todayDeaths;
+      var countryTodayRecovered = countryData.todayRecovered;
+      console.log(countryCases)
+      console.log(countryDeaths)
+      console.log(countryRecovered)
+      console.log(countryTodayCases)
+      console.log(countryTodayDeaths)
+      console.log(countryTodayRecovered)
+
+    });
+    
+
+    // for (var i = 0; i < data.length; i++) {
+    //   console.log(data[i].url);
+    //   console.log(data[i].user.login);
+    // }
     // read local storage
     // show local storage on the page
     // onclick execute the function
@@ -50,3 +101,17 @@ function getApi() {
     
 
 getApi()
+
+// function init() {
+//     var citySearchs = JSON.parse(localStorage.getItem("citySearchs"));
+//     if (citySearchs) {
+//       console.log(citySearchs);
+//       for (var i = 0; i < citySearchs.length; i++) {
+//         searchHist.append("<button>" + citySearchs[i].city + "</button>");
+//         searchHist
+//           .children()
+//           .eq()
+//           .prevObject[i].setAttribute(
+//             "class",
+//             "btn btn-secondary rounded-3 w-100 mb-2"
+//           );
