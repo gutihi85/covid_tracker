9ac2aa4a46f9182f15b27058840acd785256a0ba
Eduardo Stamm <edstamm29@gmail.com>
2021-04-24 11:06:29 -0500

    HTML style updated

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // var test1 = document.getElementById("test");
+// var countrySearch = document.getElementById("countrySearch");
 
+// countrySearch.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     console.log(countrySearch.children[0].value.toLowerCase());
+
+// });
 
 // var section1 = document.createElement("a");
 // console.log(test1);
