7806ce0137df8525b8e65c8cb335af1276c23876
Eduardo Stamm <edstamm29@gmail.com>
2021-04-23 20:53:15 -0500

    new holder update

---
+++ script.js
@@ -0,0 +1,8 @@
+// var test1 = document.getElementById("test");
+
+
+// var section1 = document.createElement("a");
+// console.log(test1);
+// test1.appendChild(section1);
+// test1.lastChild.textContent = "hola";
+// console.log(test1.lastChild);
