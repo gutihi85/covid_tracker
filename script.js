f8d51e06d7b2507633abf3e2db5b303c1470b6c6
Alejandra Gutierrez <gutihi85@gmail.com>
2021-04-27 20:46:19 -0500

    MVP

--- script.js
+++ script.js
@@ -18,7 +18,18 @@ var countryInput;
 const baseURL = "https://disease.sh/v3/covid-19/countries/";
 const newsURL = "https://content.guardianapis.com/search?q=covid&q=";
 let searchedCountryName = document.getElementById("searchedCountryName");
-
+var savedCountries = JSON.parse(localStorage.getItem('savedCountries')) || [];
+var countryTag = document.getElementById('countryTag');
+var flag = true;
+countryTag.addEventListener('click', function(event){
+    var target = event.target;
+    // console.log(target);
+    if (target.matches("button")) {
+        var countryNameTag = target.innerHTML;
+        flag = false;
+        searchCountryCovidDetails(countryNameTag,flag); 
+    } return
+});
 function retrieveGlobalCases(){
     var globalCasesUrl = `https://disease.sh/v3/covid-19/all?yesterday=false&twoDaysAgo=false&allowNull=true`;
     fetch(globalCasesUrl).then(function (response2) {
@@ -29,7 +40,6 @@ function retrieveGlobalCases(){
         alert(error)
     });
 }
-
 function putGlobalCases(globalData){
     var cases = globalData.cases;
     var deaths = globalData.deaths;
@@ -37,7 +47,6 @@ function putGlobalCases(globalData){
     var todayCases = globalData.todayCases;
     var todayDeaths = globalData.todayDeaths;
     var todayRecovered = globalData.todayRecovered;
-
     totalInfectedGlobal.textContent = cases.toLocaleString("en-US");
     totalDeathsGlobal.textContent = deaths.toLocaleString("en-US");
     totalRecoveredGlobal.textContent = recovered.toLocaleString("en-US");
@@ -45,55 +54,55 @@ function putGlobalCases(globalData){
     todayDeathsGlobal.textContent = todayDeaths.toLocaleString("en-US");
     todayRecoveredGlobal.textContent = todayRecovered.toLocaleString("en-US");
 }
-
 countrySearch.addEventListener("submit",function(event){
     event.preventDefault()
     countryInput = countrySearch.children[0].value.toUpperCase();
+    flag = true;
+    searchCountryCovidDetails(countryInput,flag);
+    countrySearch.children[0].value = "";
+});
+function searchCountryCovidDetails(countryInput,flag){
     var countryCasesUrl = `${baseURL}${countryInput}?yesterday=false&twoDaysAgo=false&strict=true&allowNull=true`
     fetch(countryCasesUrl).then(function (response3) {
         return response3.json();
     }).then(function (countryData) {
         getCountryCases(countryData, countryInput);
         getCountryNews(countryInput);
+        console.log(flag);
+        if(flag !== false){
+            toLocalStorage(countryInput)
+        }
     }).catch(error => {
         alert("Enter a valid country name");
     });
-});
-
+};
 function getCountryCases(countryData, countryInput){
     var countryCases = countryData.cases;
     var countryDeaths = countryData.deaths;
     var countryRecovered = countryData.recovered;
-    
     totalInfectedCountry.textContent = countryCases.toLocaleString("en-US");
     totalDeathsCountry.textContent = countryDeaths.toLocaleString("en-US");
     totalRecoveredCountry.textContent = countryRecovered.toLocaleString("en-US");
-
     var countryTodayCases = countryData.todayCases;
     var countryTodayDeaths = countryData.todayDeaths;
     var countryTodayRecovered = countryData.todayRecovered;
-
     if (countryTodayCases === null){
         todayInfectedCountry.textContent = "Not Reported"
     } else {
         todayInfectedCountry.textContent = countryTodayCases.toLocaleString("en-US");
     }
-
     if (countryTodayDeaths === null){
         todayDeathsCountry.textContent = "Not Reported"
     } else {
         todayDeathsCountry.textContent = countryTodayDeaths.toLocaleString("en-US");
     }
-
     if (countryTodayRecovered === null){
         todayRecoveredCountry.textContent = "Not Reported"
     } else {
         todayRecoveredCountry.textContent = countryTodayRecovered.toLocaleString("en-US");
     }
-
     searchedCountryName.innerHTML = countryInput;
 }
-
 function getCountryNews(countryInput){
     var covidNewsUrl = `${newsURL}${countryInput}&api-key=962e64d3-a7ad-4bb9-8881-a000ce805d15`;
     fetch(covidNewsUrl).then(function (response) {
@@ -104,7 +113,6 @@ function getCountryNews(countryInput){
         alert(error);
     });
 }
-
 function insertCovidNews(newsData){
     var aElement = "";
     newsHolder.innerHTML = "";
@@ -113,7 +121,7 @@ function insertCovidNews(newsData){
         var title = newsData.response.results[i].webTitle;
         var newsUrl = newsData.response.results[i].webUrl;
         aElement += `
-            <a class="button round secondary column large-3 margin-horizontal-1" target="_blank" href="${newsUrl}">
+            <a class="button round secondary column large-3 margin-horizontal-1" target="_blank" id="news-background" href="${newsUrl}">
                 ${title}
                 <div>${date}</div>
             </a>
@@ -121,5 +129,21 @@ function insertCovidNews(newsData){
     }
     newsHolder.innerHTML = aElement;
 }
-
-retrieveGlobalCases()
+function toLocalStorage(countryInput){
+    savedCountries.push(countryInput);
+    localStorage.setItem('savedCountries',JSON.stringify(savedCountries));
+    setCountryTag();
+};
+function setCountryTag(){
+    var countryTagElement = "";
+    for(var i = 0; i < savedCountries.length; i++){
+       countryTagElement += `
+       <button class="bordered rounded margin-1 padding-horizontal-1" id="button-background">
+           ${savedCountries[i]}
+       </button>
+        `
+    };
+    countryTag.innerHTML = countryTagElement;
+};
+retrieveGlobalCases();
+setCountryTag();
